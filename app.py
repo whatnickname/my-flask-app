ed29c0edcaff1dd488e255fcf49724c2654eeb07
whatnickname <qlqjs1332@gmail.com>
2025-04-17 14:52:14 +0900

    add

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on Render!"
+    return render_template("report.html")
